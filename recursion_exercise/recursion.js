f2e1e68150efeab427c53e3e2f40cd6250c7ba27
rebel_skum <jette.hernan@gmail.com>
2017-11-10 13:26:26 -0800

    update with notes

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -1,9 +1,21 @@
-function productOfArray(arr){
-  //input = arr
-  //output is an integer
-  //use for loop to enter arr
-  // grab current
-  if(arr.length === 0){
-    return 0;
+function productOfArray(arr) {
+  //base case - the array length is 0
+  //this solution works  - but it mutates the original array
+  //return arr.pop() * productOfArray(arr;
+// lets make this a pure function
+//return arr[0] * productOfArray(arr.slice(1));
+
+
+}
+function flatten(arr){
+  var newArr = []
+  for(var i = 0; i < arr.length; i++){
+    if(Array.isArray(arrr[i])){
+      flatten(arr[i]) //dont want to return the result of your recursive call
+
+    } else {
+        newArr.push(arr[i])
+    }
   }
-  
+  return newArr;
+}
