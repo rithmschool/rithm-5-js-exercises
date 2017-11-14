aaffbca4a16353b76eaef1a6fa864db6b5ad5f87
rebel_skum <jette.hernan@gmail.com>
2017-11-14 13:57:29 -0800

    finishing loginForm

--- jquery_exercise/hackernews/js/hacker.js
+++ jquery_exercise/hackernews/js/hacker.js
@@ -12,11 +12,33 @@ $(document).ready(function(){
     $("#storyForm").slideDown();
   });
 
+//console.log stories
+  // $.getJSON('https://hack-or-snooze.herokuapp.com/stories')
+  //               .then(function(response){
+  //                   console.log(response);
+  //     });
 
 
+  $("#logInForm").submit(function(e){
+       e.preventDefault()
+       $name = $("#name").val();
+       $username = $("#userName").val();
+       $password = $("#password").val();
 
-
-
+       var data = { name: $name, username: $username, password: $password };
+       //try it with getJSON
+       $.ajax({
+         method: "POST",
+         url:"https://hack-or-snooze.herokuapp.com/users",
+         data: { data }
+       })
+       .done(function(data){
+         console.log(data);
+       })
+       .fail(function(){
+         alert("OH NO, FAILED!!")
+       })
+      });
 
 
 
