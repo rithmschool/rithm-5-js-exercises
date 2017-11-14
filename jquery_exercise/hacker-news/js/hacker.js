cda51e5e8d36eccf037fa2a17861234627c6980c
Sean C. Mangosing <seanmangosing@gmail.com>
2017-11-13 16:17:10 -0800

    Updated Hack or Snooze files

--- jquery_exercise/hacker-news/js/hacker.js
+++ jquery_exercise/hacker-news/js/hacker.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  
-});
+  // $newId = "testing";
+  // $siteVal = "Yes this works";
+  console.log("Let's get ready to party with jQuery!");
+
+  $(".show_link").on('click', function(e){
+    $("form").toggleClass('visible');
+  })
+  $(".submit_btn").on('click', function (e) {
+    e.preventDefault();
+    $titleVal = $('#title').val();
+    $siteVal = $('#url').val();
+
+    $(".article_list").append('<li id=' + $titleVal + '><i class="fa fa-star-o" aria-hidden=true></i><span class="font-weight-bold"> ' + $titleVal + '</span> <span class="toSort"><small>' + $siteVal + '</small></span></li>');
+  });
+})
