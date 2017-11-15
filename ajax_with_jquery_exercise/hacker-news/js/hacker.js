aa14ee57c019b0c013ecda245bc60f71a6935612
Sean C. Mangosing <seanmangosing@gmail.com>
2017-11-15 12:59:26 -0800

    Updated Files

--- ajax_with_jquery_exercise/hacker-news/js/hacker.js
+++ ajax_with_jquery_exercise/hacker-news/js/hacker.js
@@ -2,20 +2,36 @@ $(document).ready(function () {
 
   let $sign_submit = $(".sign_sub");
   let $login_btn = $("#login-btn");
-  let story = "Story Title"
+  let story = "Story Title";
 
+//////////////////////////////////////////////// Populate the page with 10 Stories if logged in
   $.getJSON('https://hack-or-snooze.herokuapp.com/stories').then(function (response) {
     console.log(response);
     for (var i = 0; i < 10; i++) {
       let story_title = response.data[i].title;
       let story_author = response.data[i].author;
       let story_url = response.data[i].url;
-      $(".article_list").append(`<li id=${story_title} class="my-2"><i class="fa fa-star-o" aria-hidden=true></i><h6 class="font-weight-bold d-inline-block ml-1"><a href="${story_url}" target="_blank">${story_title}</a></h6> <p class="toSort d-inline-block"><small> - ${story_author})</small></p></li>`);
+      $(".article_list").append(`<li id=${story_title} class="my-2"><i class="fa fa-star-o" aria-hidden=true id="test"></i><h6 class="font-weight-bold d-inline-block ml-1"><a href="${story_url}" target="_blank">${story_title}</a></h6> <p class="toSort d-inline-block"><small> - ${story_author}</small></p></li>`);
     
     }
   })
 
-
+  //clicking on an empty star next to a story marks it as a "favorite." 
+  $("ol.article_list").on("click", "li i", function () {
+    $(this).toggleClass("fa-star-o fa-star");
+    });
+  
+  // Check if have a token and able to logged someone in
+  $.ajax({
+    url: 'https://hack-or-snooze.herokuapp.com/users/mangosing',
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem('token')
+    }
+  })
+    .then(function (response) {
+      $(".login_text").text("Logout");
+      console.log(response);
+    })
 
 
 
@@ -49,6 +65,7 @@ $(document).ready(function () {
       }
     })
     .then(function (response) {
+      $(".login_text").text("Logout");
       console.log(response);
       $.ajax({
         url: 'https://hack-or-snooze.herokuapp.com/auth',
@@ -63,8 +80,60 @@ $(document).ready(function () {
       })
       .then(function(response){
         console.log(response.data.token);
-        localStorage.setItem("test token", response.data.token);
+        localStorage.setItem("token", response.data.token);
       })
     })
   })
+
+  $(".submit_story_btn").on('click',function(e) {
+    $.ajax({
+      url: 'https://hack-or-snooze.herokuapp.com/stories?skip=0&limit=10',
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem('token')
+      },
+      method: 'POST',
+      data: {
+        data: {
+          username: 'mangosing',
+          title: 'something title',
+          author: 'Sean Mangosing',
+          url: 'http://www.google.com'
+        }
+      }
+    })
+    .then(function(response){
+      console.log(response);
+    })
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
