fc41c5af3df3ac75208b7511798c30dee2e8194d
rebel_skum <jette.hernan@gmail.com>
2017-11-14 16:26:30 -0800

    updating

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -5,17 +5,26 @@ function productOfArray(arr) {
 // lets make this a pure function
 //return arr[0] * productOfArray(arr.slice(1));
 
-
-}
-function flatten(arr){
-  var newArr = []
-  for(var i = 0; i < arr.length; i++){
-    if(Array.isArray(arrr[i])){
-      flatten(arr[i]) //dont want to return the result of your recursive call
-
-    } else {
-        newArr.push(arr[i])
-    }
+var result = 1;
+  if (arr.length === 0){
+    return result;
   }
-  return newArr;
+
+  for(var i = 0; i < arr.length; i++){
+    result *= arr[0] * productOfArray(arr.slice(0, i - 1));
+  }
+  return result;
 }
+
+// function flatten(arr){
+//   var newArr = []
+//   for(var i = 0; i < arr.length; i++){
+//     if(Array.isArray(arr[i])){
+//       flatten(arr[i]) //dont want to return the result of your recursive call
+//
+//     } else {
+//         newArr.push(arr[i])
+//     }
+//   }
+//   return newArr;
+// }
