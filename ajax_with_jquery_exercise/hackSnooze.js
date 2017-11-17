cab50b42e63f877398091a43c2be58d28cc9c252
Stephen Carrera <stephencarrera@users.noreply.github.com>
2017-11-16 16:27:53 -0800

    Anna-Brit-Trevor-Stephen2

--- ajax_with_jquery_exercise/hackSnooze.js
+++ ajax_with_jquery_exercise/hackSnooze.js
@@ -1,89 +1,100 @@
 $(function() {
-  getStories();
+  let userFavorites;
+  let token = localStorage.getItem("token");
+  let $username = localStorage.getItem("username");
+  let $password;
+
+  const $form = $("#needs-validation");
+  const $createNewUser = $("#createNewUser");
+  const $login = $("#login");
+
+  onLoadAttemptLogin();
   //load stories
+  getStories();
+
+  function onLoadAttemptLogin() {
+    if ($username && token) {
+      $("#loginNav").text(`${$username}`);
+
+      //call getUserfavorites
+    }
+  }
+
   function clearStories() {
     $("ol").empty();
   }
 
   function getStories() {
-    $.getJSON("https://hack-or-snooze.herokuapp.com/stories").then(function(
-      response
-    ) {
+    $.getJSON(
+      "https://hack-or-snooze.herokuapp.com/stories?limit=25"
+    ).then(function(response) {
       response.data.forEach(function(value, index, array) {
-        var $newTitle = $("<a>", {
-          addClass: "px-3 text-dark",
-          css: { "text-decoration": "none" },
-          attr: { href: array[index].url, target: "_blank" },
-          text: array[index].title
-        });
-
-        var $newDomain = $("<a>", {
-          addClass: "text-muted small",
-          text: "(" + getDomain(array[index].url) + ")"
-        });
-
-        var $newLi = $("<li>", {
-          addClass: "list-group-item",
-          attr: { "data-id": array[index].storyId },
-          css: { display: "list-item" }
-        });
-        var $timeCreatedByUser = $("<span>", {
-          addClass: "text-muted small",
-          text:
-            "created by " +
-            array[index].username +
-            " about " +
-            moment(array[index].updatedAt).fromNow() +
-            "  "
-        })
-
-        var $trashStory = $('<i>', {
-          addClass: "fa fa-trash-o",
-          attr: { "aria-hidden": "true"}
-        });
-
-        //append parts of list item together to create complete item
-        $newLi.append('<i class="fa fa-star-o" aria-hidden="true"></i>');
-        $newLi.append($newTitle);
-        $newLi.append($timeCreatedByUser);
-        $newLi.append($newDomain);
-        $newLi.append($trashStory);
-
-        //append new list item to document
-        $("ol").append($newLi);
-        });
+        if (array[index].username === $username) {
+          $("ol").append(`
+            <li class="list-group-item" data-id=${array[index]
+              .storyId} style="display: list-item;">
+              <i class="fa fa-star-o" aria-hidden="true"></i>
+              <a class="px-3 text-dark" href=${array[index]
+                .url} target="_blank" style="text-decoration: none;">${array[
+            index
+          ].title}</a>
+              <span class="text-muted small">created by ${array[index]
+                .username} ${moment(array[index].updatedAt).fromNow()}  </span>
+              <a class="text-muted small">(${getDomain(array[index].url)})</a>
+              <i class="fa fa-trash-o" aria-hidden="true"></i>
+            </li>
+          `);
+        } else {
+          $("ol").append(`
+            <li class="list-group-item" data-id=${array[index]
+              .storyId} style="display: list-item;">
+              <i class="fa fa-star-o" aria-hidden="true"></i>
+              <a class="px-3 text-dark" href=${array[index]
+                .url} target="_blank" style="text-decoration: none;">${array[
+            index
+          ].title}</a>
+              <span class="text-muted small">created by ${array[index]
+                .username} ${moment(array[index].updatedAt).fromNow()}  </span>
+              <a class="text-muted small">(${getDomain(array[index].url)})</a>
+              <i class="fa" aria-hidden="true"></i>
+            </li>
+          `);
+        }
       });
-    };
-  
-
+    });
+  }
 
   //login user function
-  function loginUser() {
+  function loginUser(username, password) {
     $.ajax({
       url: "https://hack-or-snooze.herokuapp.com/auth",
       method: "POST",
       data: {
         data: {
-          username: $username,
-          password: $password
+          username: username,
+          password: password
         }
       }
-    }).then(function(response) {
-      $token = response.data.token;
-      localStorage.setItem("username", $username);
-      localStorage.setItem("token", $token);
-      
-    });
+    })
+      .then(function(response) {
+        token = response.data.token;
+        localStorage.setItem("username", username);
+        localStorage.setItem("token", token);
+        $("#loginNav").text(`${username}`);
+      })
+      .then(function() {
+        $.ajax({
+          url: `https://hack-or-snooze.herokuapp.com/users/${username}`,
+          method: "GET",
+          headers: { Authorization: `Bearer ${token}` }
+        }).then(function(response) {
+          userFavorites = response.data.favorites;
+          console.log(userFavorites);
+        });
+      });
   }
 
-  //global scope -- declare username and password vars
-  let $username;
-  let $password;
-  let $token = localStorage.getItem("token");
-
   // create a user
-  const $createNewUser = $("#createNewUser");
-
   $createNewUser.on("submit", function(e) {
     e.preventDefault();
 
@@ -102,47 +113,35 @@ $(function() {
         }
       }
     }).then(function(response) {
-      $username = $newUserName;
-      $password = $newPassword;
-
-      loginUser();
+      loginUser($newUserName, $newPassword);
       clearStories();
       getStories();
     });
   });
 
   //click login to login user
-  //this calls loginUser function
-  const $login = $("#login");
 
   $login.on("click", function(e) {
     e.preventDefault();
-
-    $username = $("#username").val();
-    $password = $("#password").val();
-
-    loginUser();
+    loginUser($("#username").val(), $("#password").val());
     clearStories();
     getStories();
   });
 
-  $('ol').on('click', 'li > .fa-trash-o', function(e) {
-    let storyIdToRemove = $(e.target).parent().attr('data-id');
-    console.log(e.target)
-    console.log(storyIdToRemove)
+  //DELETE USER'S OWN STORY
+  $("ol").on("click", "li > .fa-trash-o", function(e) {
+    let storyIdToRemove = $(e.target)
+      .parent()
+      .attr("data-id");
     $.ajax({
       url: `https://hack-or-snooze.herokuapp.com/stories/${storyIdToRemove}`,
       method: "DELETE",
-      headers: {Authorization: "Bearer " + $token}
+      headers: { Authorization: `Bearer ${token}` }
     }).then(function(response) {
-      console.log('i deleted it')
-      clearStories()
-      getStories()
-    })
-  })
-
-
-  var $form = $("#needs-validation");
+      clearStories();
+      getStories();
+    });
+  });
 
   //submit a news story
   $(".submitItem").on("click", "button#submit", function(e) {
@@ -163,43 +162,16 @@ $(function() {
       $.ajax({
         url: "https://hack-or-snooze.herokuapp.com/stories",
         method: "POST",
-        headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+        headers: { Authorization: `Bearer ${token}` },
         data: {
           data: {
-            username: JSON.parse(atob($token.split(".")[1])).username,
+            username: JSON.parse(atob(token.split(".")[1])).username,
             title: titleInput,
             author: authorInput,
             url: urlInput
           }
         }
       }).then(function() {
-        // console.log($username);
-        // //create jQuery object for display
-        // var $newTitle = $("<a>", {
-        //   addClass: "px-3 text-dark",
-        //   css: { "text-decoration": "none" },
-        //   attr: { href: urlInput, target: "_blank" },
-        //   text: titleInput
-        // });
-
-        // var $newDomain = $("<a>", {
-        //   addClass: "text-muted small",
-        //   text: "(" + getDomain(urlInput) + ")"
-        // });
-
-        // var $newLi = $("<li>", {
-        //   addClass: "list-group-item",
-        //   css: { display: "list-item" }
-        // });
-
-        // //append parts of list item together to create complete item
-        // $newLi.append('<i class="fa fa-star-o" aria-hidden="true"></i>');
-        // $newLi.append($newTitle);
-        // $newLi.append($newDomain);
-
-        // //prepend new list item to document
-        // $("ol").prepend($newLi);
-
         //reset submit form
         urlInput = "";
         titleInput = "";
@@ -215,16 +187,10 @@ $(function() {
     }
   });
 
-  //toggle favorite mark
-
-  //Adding favorites to login user
-  //POST
-  //username, storyId
-  //url/users/{username}/favorites/{storyId}
-  //header: authorization token needed
+  //toggle favorite mark to server (page star does not persist)
 
   $("ol").on("click", ".fa-star, .fa-star-o", function(e) {
-    let username = JSON.parse(atob($token.split(".")[1])).username;
+    let username = JSON.parse(atob(token.split(".")[1])).username;
     var storyIdNum = $(e.target)
       .parent()
       .attr("data-id");
@@ -236,8 +202,8 @@ $(function() {
       $.ajax({
         url: `https://hack-or-snooze.herokuapp.com/users/${username}/favorites/${storyIdNum}`,
         method: "POST",
-        headers: { Authorization: "Bearer " + localStorage.getItem("token") },
-        username: JSON.parse(atob($token.split(".")[1])).username,
+        headers: { Authorization: `Bearer ${token}` },
+        username: JSON.parse(atob(token.split(".")[1])).username,
         storyId: storyIdNum
       }).then(function(response) {
         console.log(response);
@@ -247,11 +213,11 @@ $(function() {
       $.ajax({
         url: `https://hack-or-snooze.herokuapp.com/users/${username}/favorites/${storyIdNum}`,
         method: "DELETE",
-        headers: { Authorization: "Bearer " + localStorage.getItem("token") },
-        username: JSON.parse(atob($token.split(".")[1])).username,
+        headers: { Authorization: `Bearer ${token}` },
+        username: JSON.parse(atob(token.split(".")[1])).username,
         storyId: storyIdNum
       }).then(function(response) {
-        console.log("woahhhh it is gone!");
+        console.log("Successfully removed from favorites");
       });
     }
   });
@@ -314,7 +280,6 @@ $(function() {
 // remove 'www.' from hostnames
 // added usernames and timecreated to story
 
-
 // IDEAs
 /*
 infinite scroll - http://jscroll.com/
@@ -333,4 +298,5 @@ function checkForLogin() {
     $stars.hide();
     }
 }
+ONLY SHOW TRASHCANS ON STORIES USER COULD DELETE
 */
