ea9e4385f85ffc9541ea19518a40e2fefd8e2e53
Violeta Simovska <vikisimovska27@gmail.com>
2017-11-19 09:25:02 -0800

    fixed input forms

--- jquery_exercise/jQueryExercise.js
+++ jquery_exercise/jQueryExercise.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
 
+  // check if there is a token and if so....show me favorites already!
+
+  // if(localStorage.getItem('token')){
+
+  // }
+
   //****************START OF PAGE - DISPLAY THE STORIES*******************
 
   $.getJSON('https://hack-or-snooze.herokuapp.com/stories')
     .then(function(response){
+//?????????????? WHY IT WORKS SOMETIMES, AND IT DOESNT OTHER TIMES ***********************************************************     
+      $(".submitFavorite").hide();
       console.log(response);
       var arr = response.data;
       for (let i=1; i < 11; i++){
-        let title = response.data[i].title;
-        let url = response.data[i].url;
+        let title = arr[i].title;
+        let url = arr[i].url;
         $("#storyList").append(`<li><a href="${url}" target="_blank">${title}</a></li>`);    
     }
   });
@@ -18,65 +26,76 @@ $(document).ready(function() {
 
   $("#logIn").click(function(){
     //hide the story list
-    $("#storyPage").hide();
+  $("#storyPage").hide();
     //show the sign in form 
-    $("#logInForm").show();
-    });
+  $("#logInForm").show();
+  });
 
 
 //********************* LOG IN FORM (sending the username and password) *****
-   $("#logInForm").submit(function(e){
-      e.preventDefault()
-      var $name = $("#name").val();
-      var $username = $("#userName").val();
-      var $password = $("#password").val();
-     
-      localStorage.setItem("username", $username);
-      localStorage.setItem("password", $password);
+  $("#logInForm").submit(function(e){
+    e.preventDefault();
 
-      var data = { name: $name, username: $username, password: $password };
+//??????????????? IS IT GOOD PRACTICE TO SAVE THEM (NAME, USER, PASS) IN A LOCAL STORAGE ?????????????? 
+    var $name = $("#name").val();
+    var $username = $("#userName").val();
+    var $password = $("#password").val();
+  
+    localStorage.setItem("username", $username);
+    localStorage.setItem("password", $password);
 
+    var data = { name: $name, username: $username, password: $password };
+
+    $.ajax({
+      method: "POST",
+      url:"https://hack-or-snooze.herokuapp.com/users", 
+      data: { data } 
+    })
+    .done(function(data){
+      console.log(data);
       $.ajax({
         method: "POST",
-        url:"https://hack-or-snooze.herokuapp.com/users", 
-        data: { data } 
-      })
-      .done(function(data){
+        url:"https://hack-or-snooze.herokuapp.com/auth", 
+        data: { 
+          data:{ 
+            username: $username, 
+            password: $password
+          }
+        }  
+      }).done(function(data){
         console.log(data);
-        $.ajax({
-          method: "POST",
-          url:"https://hack-or-snooze.herokuapp.com/auth", 
-          data: { 
-            data:{ 
-              username: $username, 
-              password: $password
-            }
-          }  
-        }).done(function(data){
-          console.log(data);
-    
-          localStorage.setItem("username", $username);
-          localStorage.setItem("token", data.data.token);
-          //console.log("token is " + token);
-           $("#logInForm").hide();
-           // $("#storyList").fadeIn();
-           $(".nav").show();
-         
-           $("#storyPage").show();
-           //chainging the botton  to log out
-           $("#logIn").text("");
-           //$("#logIn").inner("Log Out");
-        })//?????????/DOES THE CODE BELLOW GOES HERE????????????????/
-        .fail(function(){
-          alert("OH NO, FAILED!!")
-        })
-      })
+  
+        localStorage.setItem("username", $username);
+        localStorage.setItem("token", data.data.token);
+        //console.log("token is " + token);
+         $("#logInForm").hide();
+         // $("#storyList").fadeIn();
+         $(".nav").show();
+       
+         $("#storyPage").show();
+         //chainging the botton  to log out
+         //hide the logIn btn
+          $("#logIn").toggle("hidden");
+          //show the logOut btn
+          $("#logOut").toggle("hidden");
+
+//*********CHECK THESE CODE HERE*************      
+          // newId= "logOut";
+          // $("#logIn").attr('id', 'newID');
+          // show the user their favorites
+          $(".submitFavorite").show(); 
+         //$("#logIn").inner("Log Out");
+      })//?????????/DOES THE CODE BELLOW GOES HERE????????????????/
       .fail(function(){
         alert("OH NO, FAILED!!")
       })
-    });
+    })
+    .fail(function(){
+      alert("OH NO, FAILED!!")
+    })
+  });
 
-// get the token
+  // get the token
   var token = localStorage.getItem('token');
   // get the middle part of the token by splitting on a .
   var payload = token.split(".")[1];
@@ -100,36 +119,45 @@ $(document).ready(function() {
 
 //*************** SUBMITING A NEW STORY (name, title, url)**********
 
-    $("#form").submit(function(e){
-      e.preventDefault()
-      //var $storyName = $("#storyName").val();
-      var $storyTitle = $("#title").val();
-      var $storyUrl = $("#urlLink").val();
-      var $username = localStorage.getItem('username');
-      var $author = $("#author").val();
-        
-      var data = { username: $username, title: $storyTitle, author: $author, url:$storyUrl};  
+  $("#formSubmit").submit(function(e){
+    e.preventDefault()
+    //var $storyName = $("#storyName").val();
+    var $storyTitle = $("#title").val();
+    var $storyUrl = $("#url").val();
+    var $username = localStorage.getItem('username');
+    var $author = $("#author").val();
+      
+    var data = { username: $username, title: $storyTitle, author: $author, url:$storyUrl};  
 
-      $.ajax({
-        method: "POST",
-        url:"https://hack-or-snooze.herokuapp.com/stories?skip=0&limit=10", 
-        headers: {
-        Authorization: "Bearer " + localStorage.getItem('token')
-        },
-        data: { data } 
-      })
-      .done(function(data){
-        console.log(data);
-        $("#form").hide();
-        $("#adding").toggle("hidden");
-         $('#submitLink').toggle("hidden");
-        $("#story").toggle("hidden");
-        $("#favoritesLink").toggle("hidden");
-        $("#storyList").prepend(`<li><a href="${data.data.title}" target="_blank"></li>`);
-        $("#storyList").show();
-
+    $.ajax({
+      method: "POST",
+      url:"https://hack-or-snooze.herokuapp.com/stories?skip=0&limit=10", 
+      headers: {
+      Authorization: "Bearer " + localStorage.getItem('token')
+      },
+      data: { data } 
     })
- });
+    .done(function(data){
+      console.log(data);
+      $("#form").hide();
+      $("#adding").toggle("hidden");
+       $('#submitLink').toggle("hidden");
+      $("#story").toggle("hidden");
+      $("#favoritesLink").toggle("hidden");
+      //$("#storyList").prepend(`<li><a data-storyId=${data.data.storyId} href="${data.data.url}" target="_blank">${data.data.author} - by: ${data.data.title}</li>`);
+      $("#storyList").prepend(`<li><a data-storyId=${data.data.storyId} href="${data.data.url}" target="_blank">${data.data.title}</li>`);
+      $("#storyList").show();
+    })
+  });
+
+
+//************************************** LOG OUT ****************************************
+
+  $("#logOut").click(function(){
+    $(".submitFavorit").hide();
+    $("#logOut").toggle("hidden");
+    $("#logIn").toggle("hidden");
+  });
 
 
 
@@ -138,20 +166,13 @@ $(document).ready(function() {
 
 
 
-//*************** SUBMITING THE NEW STORY (name, title, url)*****************************
 
-// $("#formSubmit").submit(function(e){
-//   e.preventDefault();
-//   $('#form').slideUp();
-//   var toAddTitle = $("#title").val();
-//   var toAddUrl = $("#urlLink").val();
-//   //add article title, link and domain
-//   $("#list").append('<li><span class="empty-star">&#9734;&nbsp</span><span class="full-star hidden">&#9733;&nbsp</span><a href="' 
-//     + toAddUrl + '">' + toAddTitle + "</a><span class='hostDomain'>(" + extractHostDomain(toAddUrl) + ")</span></li>");    
-//   $("#title").val("");
-//   $("#urlLink").val("");
-//   $("#storyName").val("");
-// }); 
+
+
+
+
+
+
 
 
 
@@ -165,6 +186,20 @@ $(document).ready(function() {
 
 //***********   CODE BELLOW NOT RELEVANT AT THE MOMENT!!!  *******************
 
+ //*************** SUBMITING THE NEW STORY (name, title, url)*****************************
+
+  // $("#formSubmit").submit(function(e){
+  //   e.preventDefault();
+  //   $('#form').slideUp();
+  //   var toAddTitle = $("#title").val();
+  //   var toAddUrl = $("#urlLink").val();
+  //   //add article title, link and domain
+  //   $("#list").append('<li><span class="empty-star">&#9734;&nbsp</span><span class="full-star hidden">&#9733;&nbsp</span><a href="' 
+  //     + toAddUrl + '">' + toAddTitle + "</a><span class='hostDomain'>(" + extractHostDomain(toAddUrl) + ")</span></li>");    
+  //   $("#title").val("");
+  //   $("#urlLink").val("");
+  //   $("#storyName").val("");
+  // }); 
   
 
   //clicking on an empty star next to a story marks it as a "favorite." 
@@ -190,25 +225,25 @@ $(document).ready(function() {
   // BONUS 2 part 1: display domain after article title: 
   // take full URL and create new var with only hostDomain
   //display this at end of every li item
-  function extractHostDomain(url) {
-    var hostname;
+  // function extractHostDomain(url) {
+  //   var hostname;
 
-    if (url.indexOf("www.") > -1) {
-        hostname = url.split('www.')[1];
+  //   if (url.indexOf("www.") > -1) {
+  //       hostname = url.split('www.')[1];
 
-    }
-    else {
-        hostname = url.split('/')[2];
-    }
+  //   }
+  //   else {
+  //       hostname = url.split('/')[2];
+  //   }
 
-    //find & remove port number
-    hostname = hostname.split(':')[0];
-    //find & remove "?"
-    hostname = hostname.split('?')[0];
-    hostname = hostname.split('/')[0];
+  //   //find & remove port number
+  //   hostname = hostname.split(':')[0];
+  //   //find & remove "?"
+  //   hostname = hostname.split('?')[0];
+  //   hostname = hostname.split('/')[0];
 
-    return hostname;
-  }
+  //   return hostname;
+  // }
 
   // BONUS 2 Part 2: when When users click on the hostname,
   // all stories with different hostnames are filtered out 
