0a0f9c31600e6157c3047e7e14ff2d428ad80784
rebel_skum <jette.hernan@gmail.com>
2017-12-08 19:36:15 -0800

    completed recursion

--- recursion_exercise/recursion.js
+++ recursion_exercise/recursion.js
@@ -17,30 +17,81 @@ function collectStrings(obj){
   //input is an obj
   //return an array of strings
   //use helper method that does the recursion in it, but build the array outside of the helper funciton
-  var strings = [];
+  let strings = [];
     function helper(obj){
-      for (var key in obj) {
+      for (let key in obj) {
               if (typeof obj[key] === "string"){
                 strings.push(obj[key]);
-              } else {
-                helper(obj[key])
               }
-          
+              if (typeof obj[key] === "object")
+                helper(obj[key]);
+              }
+    }
+     helper(obj);
+  return strings;
+}
+
+
+
+function contains(obj, val){
+  //input: object and value;
+  //returns true or false if value exists in object
+  let result = false;
+    function helper(obj, val){
+      for (let key in obj){
+          if (obj[key] === val){
+            result = true;
+            break;
+          }
+          if (typeof obj[key] === "object"){
+            helper(obj[key], val);
+          }
+      }
+    }
+    helper(obj, val);
+    return result;
+}
+
+function search(arr, val, index = 0){
+
+  if (index === arr.length) return -1;
+  if (arr[index] === val) return index;
+  else{
+    return search(arr, val, index + 1);
+  }
+
+}
+
+function binarySearch (arr, val, start = 0, end = arr.length - 1){
+  let mid = Math.floor((start + end)/ 2);
+  if (start > end) return -1;
+  if (arr[mid] === val) return mid;
+  else if (arr[mid] < val) {
+    start = mid + 1;
+    return binarySearch(arr, val, start, end);
+  }
+  else if (arr[mid] > val) {
+    end = mid - 1;
+    return binarySearch(arr, val, start, end);
+  }
+}
+
+
+function stringifyNumbers (obj){
+  let strObj = {};
+  for (let key in obj){
+    if (typeof obj[key] === "number"){
+      strObj[key] =  "" + obj[key];
+    }
+    else if (typeof obj[key] === "object" && !Array.isArray(obj[key])){
+      strObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      strObj[key] = obj[key];
     }
 
-
-  return strings;
-
+  }
+  return strObj;
 }
-}
-
-
-// function contains(){
-//   //input: object and value;
-//   //returns true or false if value exists in object
-//
-// }
-
 
 // function flatten(arr){
 //   var newArr = []
